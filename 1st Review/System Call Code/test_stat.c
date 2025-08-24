04546edd6c1cc2cf4e332a10a1f235cb8d600521
Rahul-8283 <lsrahul12@gmail.com>
2025-08-24 21:06:29 +0530

    simplified code

--- 1st Review/System Call Code/test_stat.c
+++ 1st Review/System Call Code/test_stat.c
@@ -4,34 +4,14 @@
 int main() {
     struct stat fileStat;
 
-    if (stat("test.txt", &fileStat) == -1) {
+    if (stat("test.txt", &fileStat) == -1){
         perror("stat failed");
         return 1;
     }
 
     printf("File Size: %ld bytes\n", fileStat.st_size);
     printf("Last Access: %ld\n", fileStat.st_atime);
-    printf("Permissions: %o\n", fileStat.st_mode & 0777);
+    printf("Last Modified: %ld\n", fileStat.st_mtime);
+
     return 0;
 }
-
-// #include <stdio.h>
-// #include <sys/stat.h>
-// #include <time.h>
-
-// int main() {
-//     struct stat fileStat;
-
-//     if (stat("test.txt", &fileStat) == -1) {
-//         perror("stat failed");
-//         return 1;
-//     }
-
-//     printf("File Size: %ld bytes\n", fileStat.st_size);
-//     printf("Last Access: %s", ctime(&fileStat.st_atime)); // formatted
-//     printf("Last Modified: %s", ctime(&fileStat.st_mtime));
-//     printf("Last Status Change: %s", ctime(&fileStat.st_ctime));
-
-//     return 0;
-// }
-
